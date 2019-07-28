58c14cc838613c81be9e648c5b8976ea5a3bc017
Shanu Dey <shanudey1999@gmail.com>
2019-07-28 16:16:32 +0530

    Installing package programically added

---
+++ app/src/main/java/com/example/equinoxkernelmanager/InstallActivity.java
@@ -0,0 +1,14 @@
+package com.example.equinoxkernelmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_install);
+    }
+}
