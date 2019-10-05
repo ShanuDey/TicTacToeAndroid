1baa71a0a9fe6fda929088c76de4532e3c5857c5
Shanu Dey <sd4shanudey@gmail.com>
2019-10-05 11:34:30 +0530

    matching animation changed
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/tictactoe/Matching.java
+++ app/src/main/java/com/example/tictactoe/Matching.java
@@ -21,16 +21,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Matching extends AppCompatActivity {
-    private ProgressBar progressBar;
     private TextView tv_matching;
     private String OPPONENT_ID;
     private String onlineToken;
     private User myUser;
     private String gameId;
-
     private DatabaseReference databaseReference;
-
-    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +34,9 @@ public class Matching extends AppCompatActivity {
         setContentView(R.layout.activity_matching);
 
         tv_matching = (TextView) findViewById(R.id.tv_matching);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
         databaseReference = FirebaseDatabase.getInstance().getReference();
-
         OPPONENT_ID = getIntent().getStringExtra("opponentId");
-
         Log.v("shanu","opponetId ->"+OPPONENT_ID);
-
         gameId = UserData.UID+"~"+OPPONENT_ID;
 
         databaseReference.child("online").child(OPPONENT_ID).child("status").setValue("requested");
@@ -70,13 +61,11 @@ public class Matching extends AppCompatActivity {
                            }
                         }
                     }
-
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
 
                     }
                 });
-
     }
 
     private void gameLoading(){
@@ -116,6 +105,4 @@ public class Matching extends AppCompatActivity {
         super.onBackPressed();
         cancel();
     }
-
-
 }
